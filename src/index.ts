95e811303cc56ced726e98d62ec3db4e24b7c16a
Tuan Le <latuan2906@gmail.com>
2024-06-11 19:09:41 +0700

    update db code

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,28 @@ const port = process.env.PORT || 3000;
 // database configuration
 import dotenv from 'dotenv';
 dotenv.config();
-
-// console.log();
 const dbURL = process.env.DB_URL
 const client: mongoDB.MongoClient = new mongoDB.MongoClient(dbURL as string);
-client.connect();
-console.log("Connected");
+
+async function database() {
+    try {
+        // Connect the client to the server (this will validate the connection string as well)
+        await client.connect();
+        console.log("Connected successfully to server");
+
+        // Optionally perform some database operations
+        // const db = client.db('test');
+        // const collection = db.collection('example');
+        // const count = await collection.countDocuments();
+        // console.log(`There are ${count} documents in the example collection.`);
+    } catch (err) {
+        console.error("Failed to connect to the database!", err);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+database().catch(console.error);
 
 
 app.get('/', (req: Request, res: Response) => {
