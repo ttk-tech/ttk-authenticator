f1b4debdf32a5ff90cfc61f90caf19f58c746c4d
Tuan Le <latuan2906@gmail.com>
2024-06-13 11:46:28 +0700

    test mongodb & clean architecture

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { UserService } from '../services/user-service';
+import logging from '../config/logging';
+
+export class UserController {
+  private userService: UserService;
+
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  async createUser(req: Request, res: Response) {
+    try {
+      const user = await this.userService.createUser(req.body);
+      res.status(201).send(user);
+    } catch (error) {
+      logging.error(error)
+      res.status(500).send({ message: error });
+    }
+  }
+
+  async getUserByEmail(req: Request, res: Response) {
+    try {
+      const user = await this.userService.getUserByEmail(req.body.email);
+      if (!user) {
+        logging.warn('User not found')
+        return res.status(404).send({ message: 'User not found' });
+      }
+      res.status(200).send(user);
+    } catch (error) {
+      res.status(500).send({ message: error });
+    }
+  }
+}
---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,13 @@
+// /src/repositories/userRepository.ts
+import { UserModel } from '../models/user-model';
+
+export class UserRepository {
+  async createUser(userData: { name: string; email: string; password: string }) {
+    const user = new UserModel(userData);
+    return await user.save();
+  }
+
+  async getUserByEmail(email: string) {
+    return await UserModel.findOne({ email });
+  }
+}
