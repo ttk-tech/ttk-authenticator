192d54f423c620f4197ed521d8508f7cb7c51d56
Tuan Le <latuan2906@gmail.com>
2024-06-15 14:56:58 +0700

    resolved git ignore

--- src/sample/infra/databases/prisma/migrations/20231125194419_add_on_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "refresh_token" DROP CONSTRAINT "refresh_token_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "refresh_token" ADD CONSTRAINT "refresh_token_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
