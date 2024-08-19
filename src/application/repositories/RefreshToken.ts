fdcac3ab06d15a0691bc7715a1e60955c0141678
Tuan Le <latuan2906@gmail.com>
2024-08-19 19:41:16 +0700

    add middlewares

--- src/application/repositories/RefreshToken.ts
+++ src/application/repositories/RefreshToken.ts
@@ -1,8 +1,28 @@
 import { RefreshTokenDTO } from "@domain/dtos/Authenticate/RefreshToken"
 
+/**
+ * Interface for the repository handling refresh token data 
+ * 
+ * @interface
+ */
+
 export interface IRefreshTokenRepository {
+  /**
+   * 
+   * Create a new refresh token
+   * @param {string} email - The email of the user
+   * @return {Promise<RefreshTokenDTO>} - The created refresh token
+   */
   create(userID: string): Promise<RefreshTokenDTO>
+
+  /**
+   * 
+   * Find a refresh token by ID
+   * @param {string} refreshToken - The id of the refresh token
+   * @return {Promise<RefreshTokenDTO | unknown>} - The refresh token
+   */
   findByID(refreshToken: string): Promise<RefreshTokenDTO | unknown>
+
   findByUserID(userID: string): Promise<RefreshTokenDTO | unknown>
   delete(userID: string): Promise<void>
 }
